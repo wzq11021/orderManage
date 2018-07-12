03369ed5cc2addc877376d70d4e447ac927a67da
wzqTarro <sdwzq@outlook.com>
2018-07-12 14:21:51 +0800

    同步——Tarro

--- src/main/java/com/ordermanage/controller/DataResolveController.java
+++ src/main/java/com/ordermanage/controller/DataResolveController.java
@@ -1,7 +1,9 @@
 package com.ordermanage.controller;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,23 +12,55 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.ordermanage.service.IDataResolveService;
+import com.ordermanage.util.JsonUtil;
+import com.ordermanage.util.ResultUtil;
 
 @RestController
 @RequestMapping("/dataResolve")
+@CrossOrigin
 public class DataResolveController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
-	private IDataResolveService dataResolverService;
+	private IDataResolveService dataResolveService;
 	
-	@PostMapping("/exportDataExcel")
-	public void exportDataExcel(String data, HttpServletRequest request, HttpServletResponse response){
+	/**
+	 * 解析抓取的数据格式，转为json格式
+	 * @return
+	 * @throws JsonParseException
+	 * @throws JsonMappingException
+	 * @throws IOException
+	 */
+	@GetMapping("/resolveData")
+	public ResultUtil resolveData(HttpServletResponse response) throws JsonParseException, JsonMappingException, IOException{
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		logger.info("----------->>>>>>>>>resolveData start<<<<<<<<<-------------");
+		List<Map<String,String>> list = dataResolveService.resolveData();
+		logger.info("----------->>>>>>>>>resolveData  end <<<<<<<<<-------------");
+		ResultUtil result = new ResultUtil();
+		result.setCode(0);
+		result.setMsg("ok");
+		result.setCount(1000);
+		result.setData(list);
+		return result;
+	}
+	/**
+	 * 导出Excel
+	 * @param request
+	 * @param response
+	 */
+	@GetMapping("/exportDataExcel")
+	public void exportDataExcel(HttpServletRequest request, HttpServletResponse response){
 		logger.info("--------->>>>>>>>>exportDataExcel start<<<<<<<<<<-------------");
 		try{
 			String excelName = "数据信息表" +System.currentTimeMillis() +".xls";
@@ -37,7 +71,7 @@ public class DataResolveController {
 			response.setContentType("application/vnd.ms-excel;charset=utf-8");
 			
 			//获取Excel表
-			HSSFWorkbook wb = dataResolverService.dataResolve(data);
+			HSSFWorkbook wb = dataResolveService.exportExcel();
 			if(wb==null){
 				throw new Exception("抓取数据为空");
 			}
--- src/main/java/com/ordermanage/service/IDataResolveService.java
+++ src/main/java/com/ordermanage/service/IDataResolveService.java
@@ -2,6 +2,7 @@ package com.ordermanage.service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
@@ -9,7 +10,21 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
 public interface IDataResolveService {
-	
-	HSSFWorkbook dataResolve(String data)throws JsonParseException, JsonMappingException, IOException ;
+	/**
+	 * 导出Excel
+	 * @return
+	 * @throws JsonParseException
+	 * @throws JsonMappingException
+	 * @throws IOException
+	 */
+	HSSFWorkbook exportExcel()throws JsonParseException, JsonMappingException, IOException ;
+	/**
+	 * 解析数据转换为json格式
+	 * @return
+	 * @throws JsonParseException
+	 * @throws JsonMappingException
+	 * @throws IOException
+	 */
+	List<Map<String, String>> resolveData() throws JsonParseException, JsonMappingException, IOException ;
 
 }
